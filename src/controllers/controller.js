d04c28f95ef0450664c6eb58b243e588aa8b07e2
Asiyeyurtkuran <asiyeyurtkuran@gmail.com>
2023-04-04 14:43:40 +0100

    all core req done

---
+++ src/controllers/controller.js
@@ -0,0 +1,40 @@
+const prisma = require('../utils/prisma')
+
+const getScreenById = async (req, res) => {
+    const screenId = Number(req.params.id)
+
+    const screen = await prisma.screen.findUnique({
+        where: {
+            id: screenId,
+        },
+        include: {
+            screenings:true,
+        }
+    })
+    res.json({ screen })
+}
+
+
+// ticket -> screeningId, customerId, seatNumber
+const createNewTicket = async (req, res) => {
+    const { screeningId, customerId, seatNumber} = req.body;
+
+    const ticket = await prisma.ticket.create({
+        data:{
+            screeningId,
+            customerId,
+            seatNumber,
+        },
+        include:{
+            screening:true,
+            customer:true,
+            seat: true
+        }
+    })
+    res.json({ ticket})
+}
+
+module.exports = {
+    getScreenById,
+    createNewTicket
+}
---
+++ src/routers/router.js
@@ -0,0 +1,7 @@
+const controller = require('../controllers/controller')
+const express = require('express')
+const router = express.Router()
+
+router.get('/screen/:id', controller.getScreenById)
+
+router.post('/ticket', controller.createNewTicket) 
