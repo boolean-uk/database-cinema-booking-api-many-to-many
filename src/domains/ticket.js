a70c1386f745f9596d5063fc01afb453e4e6519d
Lukas Dembicki <lad_777@hotmail.com>
2024-01-22 17:29:16 +0000

    FEAT: returning ticket. Need to build functionality for seats.

--- src/domains/ticket.js
+++ src/domains/ticket.js
@@ -1,8 +1,28 @@
 const prisma = require("../utils/prisma.js");
 
-const createTicketDB = async (id) =>
+const createTicketDB = async (screeningId, customerId) =>
     await prisma.ticket.create({
-        
+        data: {
+            screening: {
+                connect: {
+                    id: screeningId,
+                },
+            },
+            customer: {
+                connect: {
+                    id: customerId,
+                },
+            },
+        },
+        include: {
+            screening: {
+                include: {
+                    screen: true,
+                    movie: true,
+                },
+            },
+            customer: true,
+        },
     });
 
 module.exports = {
