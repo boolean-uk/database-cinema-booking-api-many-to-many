9c7159d3a5772ea82db6649550eda64dfbb98363
Chloe Zermatten <chloezermatten@gmail.com>
2024-01-22 20:10:44 +0000

    feat: get tickets including seats for a given screen with m:n explicit

--- src/domains.js/tickets_and_seats.js
+++ src/domains.js/tickets_and_seats.js
@@ -1,64 +1,51 @@
-const prisma = require("../utils/prisma")
+const prisma = require("../utils/prisma");
 
 const getTicketsWithSeatsForAScreenDb = async (id) => {
-    return await prisma.screen.findUnique({
-        where: { id },
-        include: { 
-            seats: {
-                include: {
-                    tickets: {
-                        include: {
-                            customer: {
-                                select: {
-                                    name:true
-                                }
-                            },
-                            screening: {
-                                select: {
-                                    movie: {
-                                        select: {
-                                            title: true
-                                        }
-                                    },
-                                    startsAt: true
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
-        }
-    })
-}
-
-const createTicketWithSeatsDb = async (request) => {  
-    const ticket = await prisma.ticket.create({
-        data: {
-            customer: {
-                connect: {
-                    id: request.customer
-                }
-            }, 
-            screening: {
-                connect: {
-                    id: request.screening
-                }
+  return await prisma.ticket.findMany({
+    where: {
+      seats: {
+        some: {
+          seat: {
+            screen: {
+              id: id,
             },
-            seats: {
-                connect: request.seats.map((seat) => ({number: seat.number}))
-            },
-        }, 
-        include: {
-            customer: true,
-            screening: true,
-            seats: true
-        }
-    })
-    return ticket
-}
+          },
+        },
+      },
+    }, 
+    include: {
+        seats: true
+    }
+  });
+};
+
+const createTicketWithSeatsDb = async (request) => {
+  const ticket = await prisma.ticket.create({
+    data: {
+      customer: {
+        connect: {
+          id: request.customer,
+        },
+      },
+      screening: {
+        connect: {
+          id: request.screening,
+        },
+      },
+      seats: {
+        connect: request.seats.map((seat) => ({ number: seat.number })),
+      },
+    },
+    include: {
+      customer: true,
+      screening: true,
+      seats: true,
+    },
+  });
+  return ticket;
+};
 
 module.exports = {
-    getTicketsWithSeatsForAScreenDb,
-    createTicketWithSeatsDb
-}
+  getTicketsWithSeatsForAScreenDb,
+  createTicketWithSeatsDb,
+};
