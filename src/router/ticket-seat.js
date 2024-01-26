80ef80874a64a379b6af782d4aee1d0bd2e67734
Loza_TM <lozatmamo@gmail.com>
2024-01-26 16:08:04 +0100

    comment added to route to add screen id and new ticket

--- src/router/ticket-seat.js
+++ src/router/ticket-seat.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { getTicketsWithSeatsForAScreen, createTicketWithSeats } = require("../controller/ticket-seat")
 const router = express.Router()
-router.get("/screens/:id",  getTicketsWithSeatsForAScreen)
-router.post("/tickets", createTicketWithSeats)
+router.get("/screens/:id",  getTicketsWithSeatsForAScreen) // scrren id 
+router.post("/tickets", createTicketWithSeats) // create new ticket 
 module.exports = router
