e9460b86897c9feee7a61e03bcf67958559ae806
AKINDELE AYORINDE <AKINDELEAYORINDE55@GMAIL.COM>
2024-02-21 16:14:58 +0100

    added the ticket router

---
+++ src/routers/tickets.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { generateTicket } = require("../controllers/tickets");
+
+const router = express.Router();
+
+router.post("/", generateTicket);
+
+module.exports = router;
